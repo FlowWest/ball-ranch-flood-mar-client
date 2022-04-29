6e020cd793d6c48b0b1becc031bd54484949b071
Hunter H <hunter.h.dev@gmail.com>
2022-04-29 09:52:48 -0700

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `ball-ranch-flood-mar-client`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
+
+export default config
