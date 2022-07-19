0a493c59c90b8aea38ce3c4c36044bad3d2e1a27
Hunter H <hunter.h.dev@gmail.com>
2022-07-19 10:50:08 -0700

    add sliding panel with chart to map component

--- src/components/uiComponents/map.jsx
+++ src/components/uiComponents/map.jsx
@@ -1,17 +1,45 @@
 import React, { useRef, useEffect, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
 import makeStyles from '@mui/styles/makeStyles'
-import { width } from '@mui/system'
+import { Slide, Paper, IconButton } from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
+import Chart from './chart'
 
 mapboxgl.accessToken = process.env.GATSBY_MAPBOX_ACCESS_TOKEN
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    position: 'relative',
     display: 'flex',
-    height: '500px',
+    overflow: 'hidden',
+    width: '100%',
+    height: '100%'
   },
   map: {
+    height: '600px',
+    // height: '100%',
     width: '100%',
+  },
+  slide: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    width: '45%',
+    height: '97%',
+    opacity: 0.9,
+  },
+  paper: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  paperToolBar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  chartContainer: {
+    width: '100%',
+    height: '100%',
+    padding: '1rem 2rem 5rem 2rem'
   },
 }))
 
@@ -29,7 +57,7 @@ const Map = (props) => {
         center: props.data
           ? props.data.features[0].geometry.coordinates[0][0][0]
           : undefined,
-        zoom: 12.5,
+        zoom: 11.5,
         attributionControl: false,
       })
       setMap(map)
@@ -69,8 +97,8 @@ const Map = (props) => {
       // Trigger modal when layer is clicked
       map.on('click', props.data.name, (e) => {
         console.log(`Clicked: ${props.data.name}`, e)
-        console.log(`Setting panelOpen to : ${!panelOpen}`)
-        setPanelOpen(!panelOpen)
+        console.log('open panel')
+        setPanelOpen(true)
       })
     }
   }, [map, props.data])
@@ -78,6 +106,26 @@ const Map = (props) => {
   return (
     <div className={styles.root}>
       <div id='map' ref={mapDiv} className={styles.map} />
+      <Slide in={panelOpen} direction='left' className={styles.slide}>
+        <Paper
+          elevation={3}
+          style={{ margin: 5, padding: '0 0 0 0.5rem' }}
+          className={styles.paper}
+        >
+          <div className={styles.paperToolBar}>
+            <p>Title of layer viewed</p>
+            <IconButton
+              onClick={() => setPanelOpen(false)}
+              style={{ borderRadius: 0 }}
+            >
+              <CloseIcon />
+            </IconButton>
+          </div>
+          <div className={styles.chartContainer}>
+            <Chart />
+          </div>
+        </Paper>
+      </Slide>
     </div>
   )
 }
