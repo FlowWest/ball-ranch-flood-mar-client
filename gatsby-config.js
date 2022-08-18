44b6a22d9ae8dd08d1efae4a900ae2b301eef993
Hunter H <hunter.h.dev@gmail.com>
2022-08-18 11:13:11 -0700

    Change google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [`Chau Philomene One`],
+        fonts: [`Oswald`, 'Lato'],
         display: 'swap',
       },
     },
